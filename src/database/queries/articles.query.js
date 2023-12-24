1d5e75161318eb276cdbfba65a952e2874ec5675
felipejoq <felipejofreq@gmail.com>
2023-12-24 15:46:55 -0300

    Consulta para obtener un artículo por ID

--- src/database/queries/articles.query.js
+++ src/database/queries/articles.query.js
@@ -22,3 +22,26 @@ GROUP BY a.id, a.title, a.excerpt, a.body, a.price, a.active, a.create_at, a.upd
 LIMIT $2
 OFFSET $3
 `;
+
+export const GET_ARTICLE_BY_ID = `
+SELECT
+  a.id,
+  a.title,
+  a.excerpt,
+  a.body,
+  a.price,
+  a.active,
+  a.create_at,
+  a.updated_at,
+  u.name AS user_name,
+  u.email,
+  u.image AS user_profile_img,
+  json_agg(json_build_object('id', i.id, 'url_img', i.url_img)) AS article_images
+FROM articles a
+INNER JOIN users u
+ON a.user_id = u.id
+LEFT JOIN images_article i
+ON a.id = i.article_id
+WHERE a.active = true AND a.id = $1
+GROUP BY a.id, a.title, a.excerpt, a.body, a.price, a.active, a.create_at, a.updated_at, u.name, u.email, u.image
+`;

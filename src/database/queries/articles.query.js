c6fa4fade769e030f57f3841e5f04df32003f237
felipejoq <felipejofreq@gmail.com>
2023-12-30 10:59:02 -0300

    Retorno imagen añadida con id como números enteros

--- src/database/queries/articles.query.js
+++ src/database/queries/articles.query.js
@@ -189,5 +189,5 @@ VALUES ($1, $2) RETURNING id::integer, url_img
 `;
 
 export const DELETE_IMAGE_BY_ARTICLE_IMAGE_ID = `
-DELETE FROM images_article WHERE article_id = $1 AND id = $2 RETURNING *;
+DELETE FROM images_article WHERE article_id = $1 AND id = $2 RETURNING *, id::integer, article_id::integer
 `;
